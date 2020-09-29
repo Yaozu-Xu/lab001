a96bef8e791adb66798b8e0180f7e09dc65eb86c
xuyaozu <2589231996@qq.com>
2020-09-29 10:43:12 +0100

    Arrays

--- fundamentals.js
+++ fundamentals.js
@@ -10,8 +10,22 @@ const aCar = {
         countryCode: 'WD',
         number: '1058'
     },
+    mileage: 10000,
+    color: {
+        exterior: 'red',
+        interior: {
+            texture: 'leather',
+            shade: 'cream'
+        }
+    },
+    previous_owners: [
+        { name : 'Pat Smith', address : '- 1 Main Street'},
+        { name: 'Sheila Dwyer', address: ' - 2 High Street'}
+    ],
 };
 
 console.log(aCar.owner);
 console.log(aCar.owner + ' drives a ' + aCar.type.make)
 console.log('Reg.=' + aCar.registration.year + '-', aCar.registration.countryCode + '-' + aCar.registration.number)
+console.log('it is a' + aCar.color.exterior + 'car ,', aCar.mileage + ' mileage with ' + aCar.color.interior.texture + ' interior')
+console.log('First owner : ' + aCar.previous_owners[0].name + aCar.previous_owners[0].address )
